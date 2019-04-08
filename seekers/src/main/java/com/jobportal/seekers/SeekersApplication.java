242f5f175dfa4455abf920a4e7c160d1e846fcc9
AndreaBiaggi <biaggi.jack@gmail.com>
2019-04-08 23:25:04 +0200

    add job seeker service

---
+++ seekers/src/main/java/com/jobportal/seekers/SeekersApplication.java
@@ -0,0 +1,13 @@
+package com.jobportal.seekers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeekersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeekersApplication.class, args);
+	}
+
+}
---
+++ seekers/src/test/java/com/jobportal/seekers/SeekersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jobportal.seekers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SeekersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
