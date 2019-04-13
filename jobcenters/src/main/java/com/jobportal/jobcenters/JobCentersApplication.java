619e9e30e27bb420bbbb15ece464b177e7be5557
Lorenzo Di Vito <lorenzodivito@Lorenzos-MacBook-Pro.local>
2019-04-13 15:37:18 +0200

    Add job centers service with db mapping

---
+++ jobcenters/src/main/java/com/jobportal/jobcenters/JobCentersApplication.java
@@ -0,0 +1,13 @@
+package com.jobportal.jobcenters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobCentersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobCentersApplication.class, args);
+	}
+
+}
