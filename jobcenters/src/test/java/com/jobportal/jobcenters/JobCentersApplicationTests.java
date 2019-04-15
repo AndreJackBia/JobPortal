f8ac6c5f89dd973851f37dec54a7dfec0ea55e33
Lorenzo Di Vito <lorenzodivito@Lorenzos-MacBook-Pro.local>
2019-04-15 18:24:18 +0200

    Setup of notification with the dependencies with Google API dependencies as well

--- jobcenters/src/test/java/com/jobportal/jobcenters/JobCentersApplicationTests.java
+++ jobcenters/src/test/java/com/jobportal/jobcenters/JobCentersApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class JobcentersApplicationTests {
+public class JobCentersApplicationTests {
 
 	@Test
 	public void contextLoads() {
