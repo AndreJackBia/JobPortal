18a0d4ec500dc6d9274a1a9cc93444a8c9b80281
Matteo Coppola <mcoppola94@hotmail.it>
2019-04-15 13:56:11 +0200

    Added Jobs and Search services

---
+++ jobs/src/main/java/com/jobportal/jobs/JobsApplication.java
@@ -0,0 +1,13 @@
+package com.jobportal.jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsApplication.class, args);
+	}
+
+}
