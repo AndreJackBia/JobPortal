68cec593a00f1d01c2824cc0dbdb1fe13260242d
luca-lorusso <lorusso-luca>
2019-04-16 20:58:59 +0200

    add advisor and application microservices

---
+++ advisor/src/main/java/com/jobportal/advisor/AdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.jobportal.advisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvisorApplication.class, args);
+	}
+
+}
---
+++ applications/src/main/java/com/jobportal/applications/ApplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.jobportal.applications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationsApplication.class, args);
+	}
+
+}
