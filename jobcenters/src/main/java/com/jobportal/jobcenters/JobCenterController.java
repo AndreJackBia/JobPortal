81eac8d634f090f2d84a49212b8e310d1913c864
Lorenzo Di Vito <lorenzodivito@Lorenzos-MacBook-Pro.local>
2019-04-17 20:16:31 +0200

    Add JobEntity class with logic and first creation of controller and repository

---
+++ jobcenters/src/main/java/com/jobportal/jobcenters/JobCenterController.java
@@ -0,0 +1,8 @@
+package com.jobportal.jobcenters;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JobCenterController {
+
+}
---
+++ jobcenters/src/main/java/com/jobportal/jobcenters/JobCenterRepository.java
@@ -0,0 +1,8 @@
+package com.jobportal.jobcenters;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobCenterRepository extends JpaRepository<JobCenterEntity, Long>  {
+
+
+}
