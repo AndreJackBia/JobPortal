b218d185d03647529bac936430a04f69a37229d3
Lorenzo Di Vito <lorenzodivito@Lorenzos-MacBook-Pro.local>
2019-05-19 15:47:34 +0200

    Added the most controllers inside JobCenter, missing Proxy that call Job

--- jobcenters/src/main/java/com/jobportal/jobcenters/JobCenterController.java
+++ jobcenters/src/main/java/com/jobportal/jobcenters/JobCenterController.java
@@ -5,8 +5,11 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,16 +20,75 @@ public class JobCenterController {
 	@Autowired
 	private JobCenterRepository jobCenterRepository;
 	
+	//TODO MISSING PROXY THAT CALL JOBS
+
 	@RequestMapping(value = "/api/centers", method = RequestMethod.GET)
 	public List<JobCenterEntity> getJobCenters() {
 		return jobCenterRepository.findAll();
 	}
-	
+
 	@RequestMapping(value = "/api/centers", method = RequestMethod.POST)
 	public ResponseEntity<JobCenterEntity> createJobCenter(@RequestBody JobCenterEntity jobCenterEntity)
 			throws URISyntaxException {
 		jobCenterRepository.save(jobCenterEntity);
 		return ResponseEntity.created(new URI("/api/centers" + jobCenterEntity.getId())).body(jobCenterEntity);
+	}
+
+
+	@RequestMapping(value = "/api/centers/{username}", method = RequestMethod.GET)
+	public ResponseEntity<JobCenterEntity> getJobCenter(@PathVariable String username) {
+		JobCenterEntity jobCenterEntity = jobCenterRepository.findByUsername(username);
+		if (jobCenterEntity != null) {
+			return ResponseEntity.ok().body(jobCenterEntity);
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+
+	@RequestMapping(value = "/api/centers/{username}", method = RequestMethod.DELETE)
+	public ResponseEntity<JobCenterEntity> deleteJobCenter(@RequestHeader("X-User-Header") String loggedUser,
+														   @PathVariable String username) {
+		if (!username.equals(loggedUser)) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}
+		JobCenterEntity jobCenter = jobCenterRepository.findByUsername(username);
+		//TODO DELETE BY ALL USERNAME
+		jobCenterRepository.delete(jobCenter);
+		return ResponseEntity.ok().build();
+	}
+
+	@RequestMapping(value = "/api/centers/{username}", method = RequestMethod.PUT)
+	public ResponseEntity<JobCenterEntity> updateJobCenter(@RequestHeader("X-User-Header") String loggedUser,
+														   @PathVariable String username,
+														   @RequestBody JobCenterEntity jobCenter) {
+		if (!username.equals(loggedUser)) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}
+
+		if (!username.equals(jobCenter.getUsername())) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+		if(!checkField(jobCenter)) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+		JobCenterEntity jobCenterOld = jobCenterRepository.findByUsername(username);
+		jobCenterOld.setName(jobCenter.getName());
+		jobCenterOld.setEmail(jobCenter.getEmail());
+		jobCenterRepository.save(jobCenterOld);
+		return ResponseEntity.ok(jobCenterOld);
+	}
+	
+	
+	private boolean checkField(JobCenterEntity jobCenterEntity) {
+		if(jobCenterEntity.getUsername() == null) {
+			return false;
+		}
+		if(jobCenterEntity.getEmail() == null) {
+			return false;
+		}
+		if(jobCenterEntity.getName() == null) {
+			return false;
+		}
+		return true;
 	}
 	
 }
--- jobcenters/src/main/java/com/jobportal/jobcenters/JobCenterRepository.java
+++ jobcenters/src/main/java/com/jobportal/jobcenters/JobCenterRepository.java
@@ -3,6 +3,10 @@ package com.jobportal.jobcenters;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobCenterRepository extends JpaRepository<JobCenterEntity, Long>  {
-
+	
+	JobCenterEntity findByName(String name);
+	boolean existsByName(String name);
+	JobCenterEntity findByUsername(String username);
+	void deleteByUsername(String username);
 
 }
